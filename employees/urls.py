827bd5b5cdf6f21d79f607255ceca208ac5b088d
Alehkod <kodikoleg@gmail.com>
2021-11-04 03:04:44 +0300

    Create API

--- employees/urls.py
+++ employees/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'employees'
 urlpatterns = [
-    # path('employees', views.EmployeesList.as_view(), name='employees_list')
+    path('employees', views.EmployeesList.as_view(), name='employees_list')
 ]
