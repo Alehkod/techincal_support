f1702601e54c7059ae55473bd2c0d88563c115fa
Alehkod <kodikoleg@gmail.com>
2021-11-07 22:16:40 +0300

    Uppdate admin.py(link_to parent)

--- employees/admin.py
+++ employees/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import *
 
+from django.urls import reverse
+from django.utils.html import format_html
+
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'position', 'parent', 'salary', 'paid_salary')
-    list_display_links = ('name', 'parent')
+    list_display = ('name', 'position', 'link_to_parent', 'salary', 'paid_salary')
+    list_display_links = ('name', 'link_to_parent')
     list_filter = ('position', 'level')
     search_fields = ('name', 'position')
 
@@ -15,5 +18,11 @@ class EmployeeAdmin(admin.ModelAdmin):
         queryset.update(paid_salary=0)
 
     actions = [rm_paid_salary]
+
+    def link_to_parent(self, obj):
+        link = reverse("admin:employees_employee_change", args=[obj.parent_id])
+        return format_html('<a href="{}"> {}</a>', link, obj.parent)
+
+    link_to_parent.short_description = 'Руководитель'
 
 # Register your models here.
